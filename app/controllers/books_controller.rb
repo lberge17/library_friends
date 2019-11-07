c9e3ac0e0b491cc4a0dffba69aae0efeb97d42e7
Laura Berge <lauraeberge@gmail.com>
2019-11-07 12:12:49 -0500

    started stubbing out the book controller/views.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,25 @@
 class BooksController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+    
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :author)
+    end
 end
