ee6b3f711023ff40f083197426064ef0d007bb5f
Laura Berge <lauraeberge@gmail.com>
2019-11-10 15:20:46 -0500

    added order to the book search function

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,7 @@ class Book < ApplicationRecord
     has_many :recommendations
     has_many :user_recommends, through: :recommendations, source: :user
 
-    def book_search(author: nil, title: nil)
-        Book.where("author LIKE ? AND title LIKE ?", "%#{author}%", "%#{title}%")
+    def search(author: nil, title: nil)
+        Book.where("author LIKE ? AND title LIKE ?", "%#{author}%", "%#{title}%").order("author")
     end
 end
