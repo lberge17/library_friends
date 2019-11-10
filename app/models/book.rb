49c8400782f81eed67661d17c0b4638a152fe892
Laura Berge <lauraeberge@gmail.com>
2019-11-10 10:16:15 -0500

    Added search methods in the book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,21 @@ class Book < ApplicationRecord
 
     has_many :recommendations
     has_many :user_recommends, through: :recommendations, source: :user
+
+    def search(attribute, query)
+        case attribute
+        when "author"
+            author_search(query).order("author")
+        when "title"
+            title_search(query).order("title")
+        end
+    end
+
+    def author_search(query)
+        Book.where("author LIKE ?", "%#{query}%")
+    end
+
+    def title_search(query)
+        Book.where("title LIKE ?", "%#{query}%")
+    end
 end
