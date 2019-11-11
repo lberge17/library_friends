c6a1ceb477bddb20f5f9c864c8eb613c6ac07031
Laura Berge <lauraeberge@gmail.com>
2019-11-11 09:23:27 -0500

    cleaned up library index a bit. added a class alphabetize function for books.

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,10 @@ class Book < ApplicationRecord
     has_many :user_recommends, through: :recommendations, source: :user
 
     def self.search(author: nil, title: nil)
-        Book.where("author LIKE ? AND title LIKE ?", "%#{author}%", "%#{title}%").order("author")
+        Book.where("author LIKE ? AND title LIKE ?", "%#{author}%", "%#{title}%").alphabetize
+    end
+
+    def self.alphabetize
+        self.order("author")
     end
 end
