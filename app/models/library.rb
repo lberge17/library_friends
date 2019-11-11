ce69eb43b1257d9472bcbcee595f416d18521215
Laura Berge <lauraeberge@gmail.com>
2019-11-11 12:30:02 -0500

    changed function in library model to be a scope method

--- app/models/library.rb
+++ app/models/library.rb
@@ -4,8 +4,5 @@ class Library < ApplicationRecord
 
     accepts_nested_attributes_for :book
 
-    def self.popular_books
-        # returns 5 most popular books in libraries
-        Library.order('COUNT(user_id) DESC').group('book_id').limit(5).map{|library| library.book }
-    end
+    scope :popular_books, -> { order('COUNT(user_id) DESC').group('book_id').limit(5).map{|library| library.book } }
 end
